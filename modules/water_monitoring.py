e67dca24ff998d9a13ba6cc3d7dcde3e8d9aa111
Zan <zasonce@gmail.com>
2020-07-27 14:12:45 +0200

    Documented class and its methods.

--- modules/water_monitoring.py
+++ modules/water_monitoring.py
@@ -6,15 +6,21 @@ class WaterMonitoringInstance:
     """"
     Class for loading and analysis data from Alicante and Braila. The data is
     related to plumbing/water loses.
+
+    file_name - name of the file that the data was read from
     """
     file_name = ""
     data = None
 
-    """def __init__(self, file_name, seperator):
-        self.file_name = file_name
-        self.read_data(self.file_name, seperator)"""
-
     def read_data(self, file_name, separator):
+        """
+        Method for reading data.
+
+        :param file_name: File fromn which to read.
+        :param separator: separator can take next values: "\t", ",", ";", "json". This list can be expanded...
+        """
+        self.file_name = file_name
+
         if separator == "\t":
             self.data = pandas.read_csv(file_name, sep="\t")
         elif separator == ",":
@@ -29,6 +35,11 @@ class WaterMonitoringInstance:
             raise Exception("Unknown type of separator, please remodel read_data function !")
 
     def transform_data(self, city, del_cols=False):
+        """
+        :param city: "alicante" or "braila", set it to to the place where the data originated
+        :param del_cols: This is a flag which deletes unnecessary rows -> saving RAM
+        """
+
         if city == "braila":
             self.data = self.data[(self.data.tot1 != 0) & (self.data.analog2 != 0)]
         elif city == "alicante":
@@ -44,16 +55,21 @@ class WaterMonitoringInstance:
                 new_data_f = pandas.concat([date_1_col, first_col_splitted, date_2_col, second_col_splitted], axis=1)
                 self.data = new_data_f
             else:
-                """
-                If flag is set, only the first 3 columns are returned as specified on drive
-                """
+                # If flag is set, only the first 3 columns are returned as specified on drive
                 new_data_f = pandas.concat([date_1_col, first_col_splitted], axis=1)
                 self.data = new_data_f
         else:
             raise Exception("Unknown city, modify transform_data to process other cities")
 
     def get_data_frame(self):
+        """
+        Returns instance dataframe on which all operations are performed
+        """
         return self.data
 
-    def set_data_frame(self, data):
+    def set_data_frame(self, data, file_name="Manually set"):
+        """
+        Sets the self.data to the new dataframe
+        """
         self.data = data
+        self.file_name = file_name
